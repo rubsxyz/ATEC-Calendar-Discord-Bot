f68d49f9d6cad6ec10fa9f76b641e3fec2e96288
Ruben Teixeira <devruben10@gmail.com>
2024-06-18 18:36:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import os
 from src.discord_bot import start_discord_bot
 
-# Ensure img directory exists
+# Ensure img directory exists, if not, create one
 if not os.path.exists("img"):
     os.makedirs("img")
 
