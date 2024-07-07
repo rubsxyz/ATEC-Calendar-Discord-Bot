7969b6f67aebadb23e5ef72cf0765617d02be9e5
Ruben Teixeira <devruben10@gmail.com>
2024-07-07 21:06:20 +0100

    Rollback

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-from flask import Flask, after_this_request
 import logging
 import os
 from src.discord_bot import start_discord_bot
-from threading import Thread
-from dotenv import load_dotenv
-from waitress import serve
-
-# Carregar variáveis de ambiente do .env
-load_dotenv()
 
 # Ensure img directory exists, if not, create one
 if not os.path.exists("images"):
@@ -17,23 +10,7 @@ if not os.path.exists("images"):
 logging.basicConfig(level=logging.INFO, filename='bot.log', filemode='w',
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Initialize Flask app
-app = Flask(__name__)
-
-@app.after_request
-def add_header(response):
-    response.headers['Vary'] = 'Cookie'
-    return response
-
-@app.route('/')
-def hello_world():
-    return 'Hello, Discord Bot running!'
-
-# Function to start the Flask server using waitress
-def start_flask():
-    serve(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
-# Start the bot and Flask server in different threads
+# Start the bot
 if __name__ == "__main__":
-    Thread(target=start_flask).start()
     start_discord_bot()
+
